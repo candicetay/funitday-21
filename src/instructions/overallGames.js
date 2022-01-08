e2d77295519025ea2c1297f2b8a96e4b8f9370c2
Candice Tay <candicetayjh@hotmail.sg>
2022-01-08 17:29:52 +0800

    edited games descriptions and place

--- src/instructions/overallGames.js
+++ src/instructions/overallGames.js
@@ -5,9 +5,9 @@ export default [
 		id: uuid(),
 		createdAt: '04/04/2019',
 		description:
-			'Fly again! Submit a video of a paper plane your team has folded (maximum A4 size). The plane has to fly in the air for at least 5 seconds',
+			"Fly again! Submit a video of a paper plane your team has folded (maximum A4 size) with at least 1 member's face shown. The plane has to fly in the air for at least 2 seconds,",
 		media: '/static/images/gameplay/paperplane.gif',
-		title: 'Fly High on Paper',
+		title: 'Fly High on Paper [Video]',
 		totalDownloads: '5',
 		zone: 'Generic'
 	},
@@ -15,9 +15,9 @@ export default [
 		id: uuid(),
 		createdAt: '03/04/2019',
 		description:
-			'Take a team picture that looks like you are overseas. The most creative picture wins a bonus prize.',
+			'Take a team photo (all members or excluding 1 member) that looks like you are overseas. The most creative picture wins a bonus prize.',
 		media: '/static/images/gameplay/travel.JPG',
-		title: 'We are Overseas *',
+		title: 'We are Overseas * [Picture]',
 		totalDownloads: '15',
 		zone: 'Generic'
 	},
@@ -25,19 +25,19 @@ export default [
 		id: uuid(),
 		createdAt: '04/04/2019',
 		description:
-			"It's been really hot, time for a cooling spray! Submit video evidence of water being sprayed at each of your team members.",
+			"It's been really hot, time for a cooling spray! Submit video evidence of water being sprayed at EACH of your team member's FACE.",
 		media: '/static/images/gameplay/songkran.jpg',
-		title: 'Songkran Singapore Style',
-		totalDownloads: '5',
+		title: 'Songkran Singapore Style [Video]',
+		totalDownloads: '10',
 		zone: 'Generic'
 	},
 	{
 		id: uuid(),
 		createdAt: '04/04/2019',
 		description:
-			'Review an iconic landmark or activity unique to your zone on TripAdvisor! Stuck? Fret not, unique HDB blocks count too! Best submissions win a bonus prize.',
+			'Film a team video review (vlog / tiktok) of an iconic landmark or activity unique to your zone! Stuck? Fret not, unique HDB blocks count too! Best submissions win a bonus prize.',
 		media: '/static/images/gameplay/tripadvisor_review.png',
-		title: 'Zone Travel Review *',
+		title: 'Zone Travel Review * [Video]',
 		totalDownloads: '15',
 		zone: 'Generic'
 	},
@@ -45,9 +45,9 @@ export default [
 		id: uuid(),
 		createdAt: '04/04/2019',
 		description:
-			'Miss travelling? Submit photo proof of a creative postcard written by any team member to a friend or family and post it!',
+			'Miss travelling? Submit photo proof of a creative postcard written by ANY team member to a friend or family and post it!',
 		media: '/static/images/gameplay/postcard.jpg',
-		title: 'Traveling mementos: Postcards',
+		title: 'Traveling mementos: Postcards [Photo]',
 		totalDownloads: '5',
 		zone: 'Generic'
 	},
@@ -55,19 +55,19 @@ export default [
 		id: uuid(),
 		createdAt: '31/03/2019',
 		description:
-			'Submit a video of your team doing any Tik Tok dance. The most entertaining teams gets a bonus prize.',
+			'Submit a video of your team (all members or excluding 1 member) doing any Tik Tok dance. The most entertaining team gets a bonus prize.',
 		media: '/static/images/gameplay/tiktok-dance.jpg',
-		title: 'Tik Tok Dancing Showdown *',
-		totalDownloads: '15',
+		title: 'Tik Tok Dancing Showdown * [Video]',
+		totalDownloads: '30',
 		zone: 'Generic'
 	},
 	{
 		id: uuid(),
 		createdAt: '27/03/2019',
 		description:
-			'Take a team picture with any LIVE animal of your choice! The most uncommon animal or pose would receive a bonus prize. (Birds count too!)',
+			'Take a team photo (all members or excluding 1 member) with any LIVE animal! The most uncommon animal or pose would receive a bonus prize.',
 		media: '/static/images/gameplay/partyanimals.jpg',
-		title: 'Party Animals *',
+		title: 'Party Animals * [Photo]',
 		totalDownloads: '15',
 		zone: 'Generic'
 	},
@@ -75,9 +75,9 @@ export default [
 		id: uuid(),
 		createdAt: '04/04/2019',
 		description:
-			'Have fun at the playground! Submit your most creative team picture at any playground! Best submissions win a bonus prize.',
+			'Have fun at the playground! Submit your most creative team picture (all members or excluding 1 member) at any playground! Best submissions win a bonus prize.',
 		media: '/static/images/gameplay/squidgame.jpg',
-		title: 'Squid Game hits the Playground *',
+		title: 'Squid Game hits the Playground * [Photo]',
 		totalDownloads: '15',
 		zone: 'Generic'
 	},
@@ -85,9 +85,9 @@ export default [
 		id: uuid(),
 		createdAt: '04/04/2019',
 		description:
-			'Catch no ball? How about catching food? Take a video of each team member catching Ice Gem or any Fruits with their mouth!',
+			'Catch no ball? How about catching food? Take a video of EACH team member catching Ice Gem or any Fruits with their mouth!',
 		media: '/static/images/gameplay/crop-food-catch.gif',
-		title: 'Catch it with your mouth',
+		title: 'Catch it with your mouth [Video]',
 		totalDownloads: '5',
 		zone: 'Generic'
 	},
@@ -95,9 +95,9 @@ export default [
 		id: uuid(),
 		createdAt: '04/04/2019',
 		description:
-			'Head to any Arcade outlet and play a game of Basketball! Submit a photo of the highest score achieved by your team and a video of the last 10 seconds of that game. Highest score wins a bonus prize.',
+			'Slam Dunk! Play a game of Basketball at any arcade! Submit a video of your team members playing and the highest score achieved by your team. At least the LAST 10 seconds of that game must be shown in the video. Highest score wins a bonus prize.',
 		media: '/static/images/gameplay/basketball.gif',
-		title: 'Hoop it in @ the Arcade *',
+		title: 'Hoop it in @ the Arcade * [Video]',
 		totalDownloads: '15',
 		zone: 'Generic'
 	},
@@ -105,19 +105,19 @@ export default [
 		id: uuid(),
 		createdAt: '04/04/2019',
 		description:
-			'Submit a video of your team reviewing soft boiled eggs without talking! No mouthing of words too :P',
+			'Submit a video of your team (all members or excluding 1 member) reviewing soft boiled eggs without talking! No mouthing of words too :P',
 		media: '/static/images/gameplay/notalk_eggreview.jpeg',
-		title: 'Mime food review x Eggs',
-		totalDownloads: '5',
+		title: 'Mime food review x Eggs [Video]',
+		totalDownloads: '15',
 		zone: 'Generic'
 	},
 	{
 		id: uuid(),
 		createdAt: '04/04/2019',
 		description:
-			'Tea time! Submit a video of any team member pulling Teh Tarik from 1 cup to another! We want to see foam and a real pull (from a sufficient height)!',
+			'Tea time! Submit a video of ANY team member pulling Teh Tarik from 1 cup to another! We want to see foam and a real pull (from a sufficient height)!',
 		media: '/static/images/gameplay/tehtarik.jpg',
-		title: 'Teh Tarik Master',
+		title: 'Teh Tarik Master [Video]',
 		totalDownloads: '5',
 		zone: 'Generic'
 	},
@@ -125,9 +125,9 @@ export default [
 		id: uuid(),
 		createdAt: '04/04/2019',
 		description:
-			'Are you up to the challenge? Submit a picture of a successful attempt at adding bandung to sugar cane without mixing the colours!',
-		media: '/static/images/gameplay/bandungsugarcane.jpg',
-		title: 'Bar Tender Duties',
+			"Couldn't travel to Germany last October? We have IT's own Oktoberfest! Film ANY team member chug a 330ml can (or larger) of beer empty in 15 seconds. The video must show the member opening the can and that it is empty after chugging.",
+		media: '/static/images/gameplay/beer-chug.gif',
+		title: 'Oktoberfest [Video]',
 		totalDownloads: '5',
 		zone: 'Generic'
 	},
@@ -135,9 +135,9 @@ export default [
 		id: uuid(),
 		createdAt: '04/04/2019',
 		description:
-			'Time to go hunt for any murals, graffiti and statues and submit your team picture with it! The most creative submission wins a bonus prize.',
+			'Time to go hunt for any murals, graffiti or statues and submit your team photo (all members or excluding 1 member) with it! The most creative submission wins a bonus prize.',
 		media: '/static/images/gameplay/mural_statue.jpg',
-		title: 'Art is everywhere * ',
+		title: 'Art is everywhere * [Photo]',
 		totalDownloads: '5',
 		zone: 'Generic'
 	},
@@ -145,9 +145,9 @@ export default [
 		id: uuid(),
 		createdAt: '04/04/2019',
 		description:
-			'Be a Milo athlete! Take a group picture mimicking any of the poses at https://businessmirror.com.ph/wp-content/uploads/2019/12/MILO-SEAG-Athletes-Photo.jpg Team members can pick different poses from the image!',
+			'Be a Milo athlete! Take a group shot (all members or excluding 1 member) mimicking any of the poses at https://businessmirror.com.ph/wp-content/uploads/2019/12/MILO-SEAG-Athletes-Photo.jpg Team members can pick different poses from the image!',
 		media: '/static/images/gameplay/MILO-SEAG-Athletes-Photo.jpg',
-		title: 'Team Milo Athlete',
+		title: 'Team Milo Athlete [Photo]',
 		totalDownloads: '5',
 		zone: 'Generic'
 	},
@@ -156,7 +156,7 @@ export default [
 		createdAt: '04/04/2019',
 		description: "Submit video evidence of your team's experiment!",
 		media: '/static/images/gameplay/experiment.gif',
-		title: 'Conduct any Science Experiment',
+		title: 'Conduct any Science Experiment [Video]',
 		totalDownloads: '5',
 		zone: 'Generic'
 	},
@@ -164,9 +164,9 @@ export default [
 		id: uuid(),
 		createdAt: '04/04/2019',
 		description:
-			'Take a group picture with any Haw Par Villa exhibition! Haw Par Villa is a theme park built by the founders of Tiger Balm in 1937. The park was a major local attraction in the 1970s and 1980s. The statues and dioramas can be seen as a symbolism of traditional Chinese morals and values.',
-		media: '/static/images/gameplay/west_hawparvilla.jpg',
-		title: 'Haw Par Villa',
+			"Take a group photo (all members or excluding 1 member) at Jurong Lake Gardens! Jurong Lake Gardens spans a 90-hectare space and is the third and newest of Singapore's national gardens. Visit iconic spots such as the Lone Tree, Grass mounds and Grasslands!",
+		media: '/static/images/gameplay/west_juronglakegarden2.jpg',
+		title: 'Jurong Lake Garden (Lakeside MRT) [Photo]',
 		totalDownloads: '15',
 		zone: 'West'
 	},
@@ -174,9 +174,9 @@ export default [
 		id: uuid(),
 		createdAt: '04/04/2019',
 		description:
-			'Take a group shot at the Old Bukit Timah Railway Station or Jurong Railway Line Bridge! There are two set of railways in Singapore - North-South Line and Jurong Line (total length is about 40km.) After the Tanjong Pagar Railway Station was completed in 1932, the Bukit Timah Railway Station gradually lost its importance.',
+			'Take a group shot (all members or excluding 1 member) at the Old Bukit Timah Railway Station or Jurong Railway Line Bridge! There are two set of railways in Singapore - North-South Line and Jurong Line (total length is about 40km.) After the Tanjong Pagar Railway Station was completed in 1932, the Bukit Timah Railway Station gradually lost its importance.',
 		media: '/static/images/gameplay/west_oldrailway.jpg',
-		title: 'Old Railway Line',
+		title: 'Old Railway Line (Hillview MRT or Clementi MRT) [Photo]',
 		totalDownloads: '15',
 		zone: 'West'
 	},
@@ -184,9 +184,9 @@ export default [
 		id: uuid(),
 		createdAt: '04/04/2019',
 		description:
-			'Take a group picture with any colonial bungalows along Rochester Park or Portsdown Road! The black and white houses were originally built by British colonial families between 1903 and 1928. The design combines tropical and art deco elements with a traditional Victorian style home. About 500 of these bungalows remain in Singapore.',
+			'Take a group photo (all members or excluding 1 member) with any colonial bungalows along Rochester Park or Portsdown Road (Wessex Estate)! The black and white houses were originally built by British colonial families between 1903 and 1928. The design combines tropical and art deco elements with a traditional Victorian style home. About 500 of these bungalows remain in Singapore.',
 		media: '/static/images/gameplay/west_colonialbungalow.jpg',
-		title: 'Colonial Bungalows',
+		title: 'Colonial Bungalows (One North MRT or Bouna Vista MRT) [Photo]',
 		totalDownloads: '15',
 		zone: 'West'
 	},
@@ -194,9 +194,9 @@ export default [
 		id: uuid(),
 		createdAt: '04/04/2019',
 		description:
-			"Take a group picture at Singapore's only natural hot spring park in Singapore! Believed to have healing powers, the spring was popular with villagers. After the fall of Singapore, the Japanese military seized the place and converted it into onsen baths! In the 1960s, the hot spring was frequented by gamblers for good luck. The exact source of the spring remains a mystery.",
+			"Take a group photo (all members or excluding 1 member) at Singapore's only natural hot spring park in Singapore! Believed to have healing powers, the spring was popular with villagers. After the fall of Singapore, the Japanese military seized the place and converted it into onsen baths! In the 1960s, the hot spring was frequented by gamblers for good luck. The exact source of the spring remains a mystery.",
 		media: '/static/images/gameplay/north_sembawanghotspringpark.jpg',
-		title: 'Sembawang Hot Spring Park',
+		title: 'Sembawang Hot Spring Park (Yishun MRT) [Photo]',
 		totalDownloads: '15',
 		zone: 'North'
 	},
@@ -204,9 +204,9 @@ export default [
 		id: uuid(),
 		createdAt: '04/04/2019',
 		description:
-			"Take a group picture at Singapore's 24/7 hidden adventure park. You can go fishing, prawning or have fun at a trampoline park. ORTO is also known for its 80 cents boat noodles and Thai food offerings.",
+			"Take a group shot (all members or excluding 1 member) at Singapore's 24/7 hidden adventure park. You can go fishing, prawning or have fun at a trampoline park. ORTO is also known for its 80 cents boat noodles and Thai food offerings.",
 		media: '/static/images/gameplay/north_ORTO.jpg',
-		title: 'ORTO',
+		title: 'ORTO (Khatib MRT) [Photo]',
 		totalDownloads: '15',
 		zone: 'North'
 	},
@@ -214,9 +214,9 @@ export default [
 		id: uuid(),
 		createdAt: '04/04/2019',
 		description:
-			'Take a group picture here! Treat yourselves to the beautiful view at Lower Seletar Reservoir!',
+			'Take a group shot (all members or excluding 1 member) here! Treat yourselves to the beautiful view at Lower Seletar Reservoir!',
 		media: '/static/images/gameplay/north_heritagebridge.jpg',
-		title: 'Heritage Bridge @ Lower Seletar Reservoir',
+		title: 'Heritage Bridge @ Lower Seletar Reservoir (Khatib MRT) [Photo]',
 		totalDownloads: '15',
 		zone: 'North'
 	},
@@ -224,9 +224,9 @@ export default [
 		id: uuid(),
 		createdAt: '04/04/2019',
 		description:
-			'Take a group shot with the AMK Merlions! Did you know that we have a merlion right in the heartlands of Ang Mo Kio? Built in 1998 by the Ang Mo Kio Residential Commitee, the merlions were almost forced to be removed from their locations as they are not authorised by the Singapore Tourism Board, who owns the copyright and all intellectual properties of Merlion.',
+			'Take a group shot (all members or excluding 1 member) with the AMK Merlions! Did you know that we have a merlion right in the heartlands of Ang Mo Kio? Built in 1998 by the Ang Mo Kio Residential Commitee, the merlions were almost forced to be removed from their locations as they are not authorised by the Singapore Tourism Board, who owns the copyright and all intellectual properties of Merlion.',
 		media: '/static/images/gameplay/north_east_amkmerlions.jpg',
-		title: 'Ang Mo Kio Merlions',
+		title: 'Ang Mo Kio Merlions (Ang Mo Kio MRT or Bright Hill MRT) [Photo]',
 		totalDownloads: '15',
 		zone: 'North-East'
 	},
@@ -234,9 +234,9 @@ export default [
 		id: uuid(),
 		createdAt: '04/04/2019',
 		description:
-			"Have some fun with the sculptures and take a group picture! Divided into two main areas over 24,000 square metres, the theme of the sculptural pieces focuses on marine life and fishing activities – a reference to Sengkang's history as a fishing village.",
+			"Have some fun with the sculptures and take a group photo (all members or excluding 1 member)! Divided into two main areas over 24,000 square metres, the theme of the sculptural pieces focuses on marine life and fishing activities – a reference to Sengkang's history as a fishing village.",
 		media: '/static/images/gameplay/north_east_sengkangsculpturepark.jpg',
-		title: 'Seng Kang Sculpture Park',
+		title: 'Seng Kang Sculpture Park (Seng Kang MRT or Compassvale LRT) [Photo]',
 		totalDownloads: '15',
 		zone: 'North-East'
 	},
@@ -244,9 +244,9 @@ export default [
 		id: uuid(),
 		createdAt: '04/04/2019',
 		description:
-			'Missing Thailand? Visit the Punggol Container Park! Its actual name is Social Entrepreneurship and Eco-park Development (SEED), an initiative by Social Innovation Park (SIP) to build a more inclusive and sustainable world. Each container bistro here carries a social mission and unique concept.',
+			'Missing Thailand? Visit the Punggol Container Park and take a team photo (all members or excluding 1 member)! Its actual name is Social Entrepreneurship and Eco-park Development (SEED), an initiative by Social Innovation Park (SIP) to build a more inclusive and sustainable world. Each container bistro here carries a social mission and unique concept.',
 		media: '/static/images/gameplay/north_east_punggol_east_container_park.jpg',
-		title: 'Punggol East Container Park',
+		title: 'Punggol East Container Park (Riviera LRT) [Photo]',
 		totalDownloads: '15',
 		zone: 'North-East'
 	}
